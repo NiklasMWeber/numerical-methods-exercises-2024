dc0300b3d4e57671b7322168c7eb5b32ab06200a
nikth <niklas.m.weber@gmail.com>
2024-07-09 17:20:55 +0200

    Handout 4

---
+++ src/test/java/com/niklasweber/exchangeOption/TestsExchangeOption.java
@@ -0,0 +1,23 @@
+package com.niklasweber.exchangeOption;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * This class tests the implementation of the ExchangeOption class.
+ *
+ * @author Niklas Weber
+ *
+ */
+public class TestsExchangeOption {
+
+
+	@Test
+	void test() {
+		boolean testPassed = false;
+		Assert.assertTrue(testPassed);
+	}
+
+
+}
